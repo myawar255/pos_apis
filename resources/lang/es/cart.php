2ac554f3fd6bcb07cb30c19fb9b9cd28ab4e18c8
Yawar Tariq <myawar255@gmail.com>
2025-11-16 15:49:14 +0500

    first commit

---
+++ resources/lang/es/cart.php
@@ -0,0 +1,49 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | OpenPos Cart Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during the cart for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    //==========================================
+    // Mensajes del módulo de carrito
+    //==========================================
+    'title' => 'Punto de Venta',
+
+    //==========================================
+    // Mensajes de stock
+    //==========================================
+    'available' => 'Producto disponible solo: :quantity',
+    'outstock'  => 'Producto agotado',
+
+    //==========================================
+    // Mensajes generales
+    //==========================================
+    'scan_barcode'      => 'Escanear código de barras...',
+    'received_amount'   => 'Monto recibido',
+    'confirm_pay'       => 'Recibir',
+    'cancel_pay'        => 'Cancelar',
+    'general_customer'  => 'Cliente General',
+    'product_name'      => 'Nombre del Producto',
+    'quantity'          => 'Cantidad',
+    'price'             => 'Precio',
+    'total'             => 'Total',
+    'cancel'            => 'Cancelar',
+    'checkout'          => 'Pagar',
+    'search_product'    => 'Buscar Producto',
+    'sale_completed'    => 'Venta completada correctamente.',
+    'print_receipt'     => 'Imprimir recibo',
+    'receive_payment'   => 'Recibir pago',
+    'receive_and_print' => 'Recibir e imprimir',
+    'confirm_pay_print' => 'Recibir e imprimir',
+    'view_receipt'      => 'Ver recibo',
+
+];
